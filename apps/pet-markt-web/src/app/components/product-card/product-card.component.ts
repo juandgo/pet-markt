a99a42f9141f04c7d23d71a4b18c6286e21106ab
juandgo <gojuanda01@gmail.com>
2025-05-16 09:40:51 -0500

    I'm now in the 2 part of the youtube course

---
+++ apps/pet-markt-web/src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '@prisma/client';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss',
+})
+export class ProductCardComponent {
+  product = input.required<Product>();  
+}
---
+++ apps/pet-markt-web/src/app/products/products.component.ts
@@ -0,0 +1,20 @@
+import { afterNextRender, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductStore } from '../stores/product.store';
+import { ProductCardComponent } from '../components/product-card/product-card.component';
+
+@Component({
+  selector: 'app-products',
+  imports: [CommonModule, ProductCardComponent],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss',
+})
+export class ProductsComponent {
+  productStore = inject(ProductStore);
+
+  constructor() {
+    afterNextRender(() => {
+      this.productStore.loadProducts();
+    });
+  }
+}
